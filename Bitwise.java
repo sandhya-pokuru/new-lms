ec3c79a0ba60e830b3ac309159bc32439125f866
sandhyapokuru <sandhyau1018@gmail.com>
2023-09-21 10:34:52 +0530

    commit

---
+++ Bitwise.java
@@ -0,0 +1,28 @@
+class Bitwise{
+		public static void main (String[]args){
+		boolean a = true;
+		boolean b = false;
+		System.out.println("truth table of & operator");
+		System.out.println("..........................");
+		System.out.println( b&b);
+		System.out.println(b&a);
+		System.out.println(a&b);
+		System.out.println(a&a);
+		System.out.println(); 
+		System.out.println("truth table of | operator");
+		System.out.println("..........................");
+		System.out.println( b|b);
+		System.out.println(b|a);
+		System.out.println(a|b);
+		System.out.println(a|a);
+		System.out.println(); 
+		System.out.println("truth table of ^ operator");
+		System.out.println("..........................");
+		System.out.println( b^b);
+		System.out.println(b^a);
+		System.out.println(a^b);
+		System.out.println(a^a);
+		System.out.println(); 
+
+		}
+  }
